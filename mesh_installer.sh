f391954b957c953572cd2f816f3e328cfc5dec2a
binaryhellstorm <binaryhellstorm@gmail.com>
2025-07-21 21:49:13 -0400

    Update mesh_installer.sh

--- mesh_installer.sh
+++ mesh_installer.sh
@@ -1,8 +1,8 @@
 echo 'deb http://download.opensuse.org/repositories/network:/Meshtastic:/beta/Raspbian_12/ /' | sudo tee /etc/apt/sources.list.d/network:Meshtastic:beta.list
 curl -fsSL https://download.opensuse.org/repositories/network:Meshtastic:beta/Raspbian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/network_Meshtastic_beta.gpg > /dev/null
 sudo apt update
-sudo apt install meshtasticd
 sudo apt-get -y upgrade
+sudo apt install meshtasticd
 sudo wget –O /etc/meshtasticd/config.d https://github.com/binaryhellstorm/meshconfig/raw/refs/heads/main/NebraHat_1W.yaml
 sudo wget –O /etc/meshtasticd/ https://github.com/binaryhellstorm/meshconfig/raw/refs/heads/main/config.yaml
 sudo wget –O /etc/meshtasticd/ https://github.com/binaryhellstorm/meshconfig/raw/refs/heads/main/config.txt
